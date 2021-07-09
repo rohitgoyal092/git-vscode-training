89a287f7d4b96304a395f54edd921ce95a93ba31
rohitgoyal092 <rohit.goyal@sprinklr.com>
2021-07-09 17:55:53 +0530

    Swiggy Project - Improved Scrolling, fixed z index issue, resolved scrolling bug, resolved one alignment bug

--- SwiggyProj/swiggy.js
+++ SwiggyProj/swiggy.js
@@ -1,30 +1,24 @@
 let scrollBox = document.querySelector(".containImg");
 let leftButton = document.querySelector(".leftButton");
 let rightButton = document.querySelector(".rightButton");
+let curItem = 0;
 let imgList = scrollBox.children;
+let DURATION = 250;
+let scrolles = [];
 
-// window.addEventListener("load", function (event) {
-//   if (rightScrollable(scrollBox) === false) {
-//     rightButton.hidden = true;
-//   }
-//   leftButton.hidden = true;
-// });
-
-function rightScrollable(ele, len) {
-  let scrolled = ele.scrollLeft;
+function rightScrollable(ele, off = 0) {
+  let scrolled = ele.scrollLeft + off;
   let maxScrollLeft = ele.scrollWidth - ele.clientWidth;
-  // console.log("scrolled : ", scrolled);
-  // console.log("total : ", maxScrollLeft);
-  if (maxScrollLeft - scrolled >= 0) {
+  if (maxScrollLeft - scrolled > Number.EPSILON) {
     return true;
   }
-  // console.log("cannot!!");
+  console.log("NO");
   return false;
 }
 
-function leftScrollable(ele) {
+function leftScrollable(ele, off = 0) {
   let scrolled = ele.scrollLeft;
-  if (scrolled > 0) {
+  if (scrolled - off > Number.EPSILON) {
     return true;
   }
   return false;
@@ -32,62 +26,86 @@ function leftScrollable(ele) {
 
 function getJumpLength(imgList) {
   return (
-    parseFloat(window.getComputedStyle(imgList[0]).getPropertyValue("width")) +
     parseFloat(
-      window.getComputedStyle(imgList[0]).getPropertyValue("margin-left")
+      window.getComputedStyle(imgList[curItem]).getPropertyValue("width")
     ) +
     parseFloat(
-      window.getComputedStyle(imgList[0]).getPropertyValue("margin-right")
+      window.getComputedStyle(imgList[curItem]).getPropertyValue("margin-left")
+    ) +
+    parseFloat(
+      window.getComputedStyle(imgList[curItem]).getPropertyValue("margin-right")
     )
   );
 }
 
+window.addEventListener("load", function (event) {
+  leftButton.style.hidden = "true";
+});
+
 rightButton.addEventListener("click", function (event) {
-  if (!rightScrollable(scrollBox, getJumpLength(imgList))) return;
-  scrollBox.scrollLeft += getJumpLength(imgList);
+  if (!rightScrollable(scrollBox)) return;
+  let scrollAmount = Math.min(
+    getJumpLength(imgList),
+    scrollBox.scrollWidth - scrollBox.clientWidth - scrollBox.scrollLeft
+  );
+  scrolles.push(scrollAmount);
+  scrollBox.scrollBy({
+    top: 0,
+    left: scrollAmount,
+    behavior: "smooth",
+  });
+  curItem++;
 });
 
 leftButton.addEventListener("click", function (event) {
-  if (!leftScrollable(scrollBox, getJumpLength(imgList))) return;
-  scrollBox.scrollLeft -= getJumpLength(imgList);
+  if (!leftScrollable(scrollBox)) return;
+  curItem--;
+  scrollBox.scrollBy({
+    top: 0,
+    left: -scrolles.pop(),
+    behavior: "smooth",
+  });
 });
 
-rightButton.addEventListener("mouseover", function (event) {
+rightButton.addEventListener("mouseenter", function (event) {
   rightArrow = document.querySelector(".rightButton .arrowClass");
   rightArrow.animate(
     [
       { transform: "translateX(0px)" },
+      { transform: "translateX(1px)" },
       { transform: "translateX(2px)" },
-      { transform: "translateX(4px)" },
-      { transform: "translateX(8px)" },
+      { transform: "translateX(3px)" },
     ],
-    { duration: 250, fill: "forwards" }
+    { duration: DURATION, fill: "forwards" }
   );
 });
+
 rightButton.addEventListener("mouseleave", function (event) {
   rightArrow = document.querySelector(".rightButton .arrowClass");
   rightArrow.animate([{ transform: "translateX(0px)" }], {
-    duration: 1,
+    duration: DURATION,
     fill: "forwards",
   });
+  rightArrow.style.zIndex = "auto";
 });
 
-leftButton.addEventListener("mouseover", function (event) {
+leftButton.addEventListener("mouseenter", function (event) {
   leftArrow = document.querySelector(".leftButton .arrowClass");
   leftArrow.animate(
     [
       { transform: "translateX(0px)" },
+      { transform: "translateX(-1px)" },
       { transform: "translateX(-2px)" },
-      { transform: "translateX(-4px)" },
-      { transform: "translateX(-8px)" },
+      { transform: "translateX(-3px)" },
     ],
-    { duration: 250, fill: "forwards" }
+    { duration: DURATION, fill: "forwards" }
   );
 });
 leftButton.addEventListener("mouseleave", function (event) {
   leftArrow = document.querySelector(".leftButton .arrowClass");
   leftArrow.animate([{ transform: "translateX(0px)" }], {
-    duration: 1,
+    duration: DURATION,
     fill: "forwards",
   });
+  leftArrow.style.zIndex = "auto";
 });
