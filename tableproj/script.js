c4dccfa169c72db62e55b79ec7682cc033223bc3
rohitgoyal092 <rohit.goyal@sprinklr.com>
2021-07-13 20:35:54 +0530

    Multigrid - Removed code redundancy from eventhandlers

--- tableproj/script.js
+++ tableproj/script.js
@@ -1,4 +1,4 @@
-let defaultSpecs = {
+const defaultSpecs = {
   rows: 20,
   cols: 20,
   fixRow: 5,
@@ -7,17 +7,15 @@ let defaultSpecs = {
   M: 15,
 };
 
+const cellWidth = 80,
+  cellHeight = 40;
+
 let inputRows = document.querySelector(".labelBox:nth-child(1) .ibox");
 let inputCols = document.querySelector(".labelBox:nth-child(2) .ibox");
 let inputFixRows = document.querySelector(".labelBox:nth-child(3) .ibox");
 let inputFixCols = document.querySelector(".labelBox:nth-child(4) .ibox");
 let inputN = document.querySelector(".labelBox:nth-child(5) .ibox");
 let inputM = document.querySelector(".labelBox:nth-child(6) .ibox");
-
-let cellWidth = "80px",
-  cellHeight = "40px";
-let cellWidthNP = 80,
-  cellHeightNP = 40;
 
 let specs = Object.assign({}, defaultSpecs);
 
@@ -27,24 +25,8 @@ let bottomLeft = document.querySelector(".bottomLeft");
 let bottomRight = document.querySelector(".bottomRight");
 
 function getStyling(i, j, specs) {
-  let s = "";
-  s += `<div class = "cellClass">${i}, ${j}</div>`;
+  let s = `<div class = "cellClass">${i}, ${j}</div>`;
   return s;
-}
-
-function resetStyle() {
-  if (topLeft.hasAttribute(style)) {
-    topLeft.removeAttribute(style);
-  }
-  if (topRight.hasAttribute(style)) {
-    topRight.removeAttribute(style);
-  }
-  if (bottomLeft.hasAttribute(style)) {
-    bottomLeft.removeAttribute(style);
-  }
-  if (bottomRight.hasAttribute(style)) {
-    bottomRight.removeAttribute(style);
-  }
 }
 
 function setNull(corner) {
@@ -54,19 +36,22 @@ function setNull(corner) {
   );
 }
 
-function styleDivs(specs) {
+function resizeGrid(specs) {
   let grid = document.querySelector(".grid");
-  let w = cellWidthNP * Math.max(specs.M, specs.fixCol + 1);
-  w = Math.min(w, cellWidthNP * specs.cols);
-  let h = cellHeightNP * Math.max(specs.N, specs.fixRow + 1);
-  h = Math.min(h, cellHeightNP * specs.rows);
+  let w = cellWidth * Math.max(specs.M, specs.fixCol + 1);
+  w = Math.min(w, cellWidth * specs.cols);
+  let h = cellHeight * Math.max(specs.N, specs.fixRow + 1);
+  h = Math.min(h, cellHeight * specs.rows);
 
   grid.setAttribute("style", `width:${w}px;height:${h}px`);
-  // resetStyle();
+}
+
+function styleDivs(specs) {
+  resizeGrid(specs);
 
   if (Math.min(specs.fixCol, specs.fixRow) > 0) {
-    topLeft.style.gridTemplateColumns = `repeat(${specs.fixCol}, ${cellWidth})`;
-    topLeft.style.gridTemplateRows = `repeat(${specs.fixRow}, ${cellHeight})`;
+    topLeft.style.gridTemplateColumns = `repeat(${specs.fixCol}, ${cellWidth}px)`;
+    topLeft.style.gridTemplateRows = `repeat(${specs.fixRow}, ${cellHeight}px)`;
     topLeft.style.removeProperty("border");
   } else {
     setNull(topLeft);
@@ -75,18 +60,18 @@ function styleDivs(specs) {
   if (Math.min(specs.cols - specs.fixCol, specs.fixRow) > 0) {
     topRight.style.gridTemplateColumns = `repeat(${
       specs.cols - specs.fixCol
-    }, ${cellWidth})`;
-    topRight.style.gridTemplateRows = `repeat(${specs.fixRow}, ${cellHeight})`;
+    }, ${cellWidth}px)`;
+    topRight.style.gridTemplateRows = `repeat(${specs.fixRow}, ${cellHeight}px)`;
     topRight.style.removeProperty("border");
   } else {
     setNull(topRight);
   }
 
   if (Math.min(specs.fixCol, specs.rows - specs.fixRow) > 0) {
-    bottomLeft.style.gridTemplateColumns = `repeat(${specs.fixCol}, ${cellWidth})`;
+    bottomLeft.style.gridTemplateColumns = `repeat(${specs.fixCol}, ${cellWidth}px)`;
     bottomLeft.style.gridTemplateRows = `repeat(${
       specs.rows - specs.fixRow
-    }, ${cellHeight})`;
+    }, ${cellHeight}px)`;
     bottomLeft.style.removeProperty("border");
   } else {
     setNull(bottomLeft);
@@ -95,10 +80,10 @@ function styleDivs(specs) {
   if (Math.min(specs.cols - specs.fixCol, specs.rows - specs.fixRow) > 0) {
     bottomRight.style.gridTemplateColumns = `repeat(${
       specs.cols - specs.fixCol
-    }, ${cellWidth})`;
+    }, ${cellWidth}px)`;
     bottomRight.style.gridTemplateRows = `repeat(${
       specs.rows - specs.fixRow
-    }, ${cellHeight})`;
+    }, ${cellHeight}px)`;
     bottomRight.style.removeProperty("border");
   } else {
     setNull(bottomRight);
@@ -168,6 +153,7 @@ let topRightScrolled = false,
   bottomLeftScrolled = false;
 
 topRight.onscroll = function (event) {
+  console.log("topRight", topRightScrolled);
   if (!topRightScrolled) {
     bottomRightScrolled = true;
     bottomRight.scrollLeft = this.scrollLeft;
@@ -175,6 +161,7 @@ topRight.onscroll = function (event) {
   topRightScrolled = false;
 };
 bottomRight.onscroll = function (event) {
+  console.log("bottomRight", bottomRightScrolled);
   if (!bottomRightScrolled) {
     topRightScrolled = true;
     bottomLeftScrolled = true;
@@ -213,80 +200,47 @@ function getVal(str) {
   return parseInt(str);
 }
 
-inputRows.addEventListener("input", function (event) {
-  if (!checkValidNumber(this.value, event)) {
-    specs.rows = 0;
-    this.value = 0;
-  } else if (this.value.length < 1) {
-    specs.rows = 0;
+function getInputFieldValue(inputField, event) {
+  let retVal = 0;
+  if (!checkValidNumber(inputField.value, event)) {
+    retVal = 0;
+    inputField.value = 0;
+  } else if (inputField.value.length < 1) {
+    retVal = 0;
   } else {
-    specs.rows = parseInt(this.value);
+    retVal = getVal(inputField.value);
   }
-  specs.rows = Math.max(specs.rows, specs.fixRow);
+  return retVal;
+}
+
+inputRows.addEventListener("input", function (event) {
+  specs.rows = Math.max(getInputFieldValue(this, event), specs.fixRow);
   resize(specs);
 });
 
 inputCols.addEventListener("input", function (event) {
-  if (!checkValidNumber(this.value, event)) {
-    specs.cols = 0;
-    this.value = 0;
-  } else if (this.value.length < 1) {
-    specs.cols = 0;
-  } else {
-    specs.cols = parseInt(this.value);
-  }
-  specs.cols = Math.max(specs.cols, specs.fixCol);
+  specs.cols = Math.max(getInputFieldValue(this, event), specs.fixCol);
   resize(specs);
 });
 
 inputFixRows.addEventListener("input", function (event) {
-  if (!checkValidNumber(this.value, event)) {
-    specs.fixRow = 0;
-    this.value = 0;
-  } else if (this.value.length < 1) {
-    specs.fixRow = 0;
-  } else {
-    specs.fixRow = parseInt(this.value);
-  }
+  specs.fixRow = getInputFieldValue(this, event);
   specs.rows = Math.max(getVal(inputRows.value), specs.fixRow);
   resize(specs);
 });
 
 inputFixCols.addEventListener("input", function (event) {
-  if (!checkValidNumber(this.value, event)) {
-    specs.fixCol = 0;
-    this.value = 0;
-  } else if (this.value.length < 1) {
-    specs.fixCol = 0;
-  } else {
-    specs.fixCol = parseInt(this.value);
-  }
+  specs.fixCol = getInputFieldValue(this, event);
   specs.cols = Math.max(getVal(inputCols.value), specs.fixCol);
   resize(specs);
 });
 
 inputN.addEventListener("input", function (event) {
-  if (!checkValidNumber(this.value, event)) {
-    specs.N = 0;
-    this.value = 0;
-  } else if (this.value.length < 1) {
-    specs.N = 0;
-  } else {
-    specs.N = parseInt(this.value);
-  }
-  specs.N = Math.min(specs.N, specs.rows);
+  specs.N = Math.min(getInputFieldValue(this, event), specs.rows);
   resize(specs);
 });
 
 inputM.addEventListener("input", function (event) {
-  if (!checkValidNumber(this.value, event)) {
-    specs.M = 0;
-    this.value = 0;
-  } else if (this.value.length < 1) {
-    specs.M = 0;
-  } else {
-    specs.M = parseInt(this.value);
-  }
-  specs.M = Math.min(specs.M, specs.cols);
+  specs.M = Math.min(getInputFieldValue(this, event), specs.cols);
   resize(specs);
 });
